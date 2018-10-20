0fdffa7407be8c59b4e4aafab56c7fe21562997e
Geraldavila <geraldine.davila.pico@gmail.com>
2018-10-19 23:33:16 -0700

    Assignment 2. Factory Class

---
+++ PetStoreApp/Prod_Maker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetStoreApp
+{
+    static class Prod_Maker
+    {
+        public static Product CreateProduct(string name, string description, decimal price, int initial_quantity = 0, TypeOfPet pet = TypeOfPet.Dog)
+        {
+            var product = new Product
+            {
+                Name = name,
+                Description = description,
+                Price = price,
+                Pet = pet,
+            };
+            
+            if (initial_quantity > 0)
+            {
+                product.Input(initial_quantity);
+            }
+            return product;
+        }
+    }
+}
--- PetStoreApp/Program.cs
+++ PetStoreApp/Program.cs
@@ -6,39 +6,73 @@ namespace PetStoreApp
     {
         static void Main(string[] args)
         {
-            var prod = new Product
+            Console.WriteLine("***************************");
+            Console.WriteLine("PetStore");
+            Console.WriteLine("***************************");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Register new product");
+            Console.WriteLine("2. Print all Products"); //WriteLine: write the whole thing and go to the next line
+            Console.Write("Please select an option: ");   //Write: will write andd stay in the same place          
+            var option = Console.ReadLine();
+
+            switch (option)
             {
-                Name = "Treats",
-                Description = "Chiken flavor treats",
-                Price = 15.00M,
-                Pet = TypeOfPet.Dog,
-            };
-            prod.Input(50);
-            prod.Output(20);
+                case "0":
+                    return;
+                    
+                case "1":
+                    Console.Write("Name: ");
+                    var name = Console.ReadLine();
+                    Console.Write("Description of the product: ");
+                    var description = Console.ReadLine();
+                    Console.Write("Enter the price of the product: ");
+                    var price = Console.ReadLine();
+                    var pet = Enum.GetNames(typeof(TypeOfPet));
 
-            Console.WriteLine($"PD: {prod.ID}, Name: {prod.Name}, Pet: {prod.Pet}, Price: {prod.Price:C}, Quantity: {prod.Quantity}");
-
-            var prod2 = new Product
-            {
-                Name = "Leash",
-                Description = "Large leash with lock",
-                Price = 20.00M,
-                Pet = TypeOfPet.Dog,
-            };
-
-            Console.WriteLine($"PD: {prod2.ID}, Name: {prod2.Name}, Pet: {prod2.Pet}, Price: {prod2.Price:C}");
-
-            var prod3 = new Product
-            {
-                Name = "Brush",
-                Description = "Soft large size brush",
-                Price = 8.00M,
-                Pet = TypeOfPet.Cat,
-            };
-
-            Console.WriteLine($"PD: {prod3.ID}, Name: {prod3.Name}, Pet: {prod3.Pet}, Price: {prod3.Price:C}");
+                    for (var i = 0; i < pet.Length; i++)
+                    {
+                        Console.WriteLine($"{i+1}.{pet[i]}");
+                    }
+                    Console.Write("Select a pet: ");
+                    var petOption = Console.ReadLine();
+                    break;
+            }
 
 
+            #region Old 
+            /* var prod = new Product
+             {
+                 Name = "Treats",
+                 Description = "Chiken flavor treats",
+                 Price = 15.00M,
+                 Pet = TypeOfPet.Dog,
+             };
+             prod.Input(50);
+             prod.Output(20);
+
+             Console.WriteLine($"PD: {prod.ID}, Name: {prod.Name}, Pet: {prod.Pet}, Price: {prod.Price:C}, Quantity: {prod.Quantity}");
+
+             var prod2 = new Product
+             {
+                 Name = "Leash",
+                 Description = "Large leash with lock",
+                 Price = 20.00M,
+                 Pet = TypeOfPet.Dog,
+             };
+
+             Console.WriteLine($"PD: {prod2.ID}, Name: {prod2.Name}, Pet: {prod2.Pet}, Price: {prod2.Price:C}");
+
+             var prod3 = new Product
+             {
+                 Name = "Brush",
+                 Description = "Soft large size brush",
+                 Price = 8.00M,
+                 Pet = TypeOfPet.Cat,
+             };
+
+             Console.WriteLine($"PD: {prod3.ID}, Name: {prod3.Name}, Pet: {prod3.Pet}, Price: {prod3.Price:C}");
+             */
+            #endregion
         }
     }
 }
